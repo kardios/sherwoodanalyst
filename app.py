1514bd7522df33cb8ddcf7e8a6c77ae18319ec4a
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-11-29 00:24:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import os
 import time
 import telebot
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from openai import OpenAI
 from pypdf import PdfReader
 from st_copy_to_clipboard import st_copy_to_clipboard
@@ -13,47 +16,78 @@ bot = telebot.TeleBot(bot_token)
 
 # Retrieve the API keys from the environment variables
 CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
+CLAUDE_API_KEY = os.environ["ANTHROPIC_API_KEY"]
+GEMINI_API_KEY = os.environ["GOOGLE_API_KEY"]
+MISTRA_API_KEY = os.environ["MISTRAL_API_KEY"]
+
 client = OpenAI(api_key=CLIENT_API_KEY)
+anthropic = Anthropic(api_key=CLAUDE_API_KEY)
+genai.configure(api_key=GEMINI_API_KEY)
+mistral = Mistral(api_key=MISTRA_API_KEY)
+
+safety_settings = {
+  HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+}
+
+generation_config = genai.GenerationConfig(
+  candidate_count = 1,
+  temperature = 0,
+)
 
 st.set_page_config(page_title="Sherwood Analyst", page_icon=":desktop_computer:",)
 st.write("**Sherwood Analyst**, your smart analyst")
 
-Option_Input = st.selectbox("How will I receive your input?", ('Upload a pdf','Enter free text'))
+research_topic = st.text_input("What is my research topic?", "Analyse the implications of Vietnam\'s Doi Moi reforms")
 
-instruction = "Read the input below. Analyse the implications at the regional and international level."
-
-if Option_Input == "Upload a pdf":
-  uploaded_file = st.file_uploader("Upload a PDF to analyse:", type = "pdf")
-  raw_text = ""
-  if uploaded_file is not None:
-    try:
-      doc_reader = PdfReader(uploaded_file)
-      with st.spinner("Extracting from PDF document..."):
-        for i, page in enumerate(doc_reader.pages):
-          text = page.extract_text()
-          if text:
-            raw_text = raw_text + text + "\n"
-    except:
-      st.error(" Error occurred when loading document", icon="🚨")
-elif Option_Input == "Enter free text":
-  raw_text = ""
-  input_text = st.text_area("Enter the text you would like me to analyse and click **Let\'s Go! :desktop_computer:**")
-  if st.button("Let\'s Go! :desktop_computer:"):
-    raw_text = input_text + "\n..."
-
-if raw_text.strip() != "":
-  with st.spinner("Running AI Model..."):
-    start = time.time()
-    input = instruction + "\n\n" + raw_text
-    response = client.chat.completions.create(model="o1-preview", 
-                                              messages=[{"role": "user", "content": input}])
-    output_text = response.choices[0].message.content
-    end = time.time()
-
-  with st.expander("Sherwood Analyst", expanded = True):
-    st.write(output_text)
-    st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+if st.button("Let\'s Go! :rocket:") and research_topic.strip()!="":
   
-  st_copy_to_clipboard(output_text)
-    
-  bot.send_message(chat_id=recipient_user_id, text="Sherwood Analyst")
+  with st.spinner("Running AI Model..."):
+
+    start = time.time()
+    input = research_topic
+    response = client.chat.completions.create(model="o1-preview", messages=[{"role": "user", "content": input}])
+    o1_preview_raw = response.choices[0].message.content
+    end = time.time()
+    with st.expander("Raw o1-preview output"):
+      st.markdown(o1_preview_raw)
+      st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+      st_copy_to_clipboard(o1_preview_raw)
+    st.snow()
+
+    start = time.time()
+    input = "Rewrite the answer below into a series of paragraphs, without headings:\n\n" + research_topic
+    response = client.chat.completions.create(model="gpt-4o-2024-11-20", messages=[{"role": "user", "content": input}])
+    rewrite_raw = response.choices[0].message.content
+    end = time.time()
+    with st.expander("Raw gpt-4o rewritten"):
+      st.markdown(rewrite_raw)
+      st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+      st_copy_to_clipboard(rewrite_raw)
+    st.snow()
+
+    start = time.time()
+    input = "Research facts, trends and statistics relevant to the topic:\n\n" + research_topic
+    gemini = genai.GenerativeModel("gemini-1.5-pro-002")
+    response = gemini.generate_content(input, safety_settings = safety_settings, generation_config = generation_config, tools = "google_search_retrieval")
+    research_raw = response.text
+    with st.expander("Raw research result"):
+      st.markdown(research_raw)
+      st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+      st_copy_to_clipboard(research_raw)
+    st.snow()
+
+    start = time.time()
+    input = "Update <answer> with <research>:\n\n"
+    input = input + "<answer>\n\n" + rewrite_raw + "\n\n</answer>\n\n"
+    input = input + "<research>\n\n" + research_raw + "\n\n</research>\n\n"
+    response = client.chat.completions.create(model="gpt-4o-2024-11-20", messages=[{"role": "user", "content": input}])
+    updated_answer = response.choices[0].message.content
+    end = time.time()
+    with st.expander("Updated answer"):
+      st.markdown(updated_answer)
+      st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+      st_copy_to_clipboard(updated_answer)
+    st.snow()
